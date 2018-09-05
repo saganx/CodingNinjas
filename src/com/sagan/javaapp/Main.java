5c5ed860dc8b5fb684fff7d255546b008841a23e
Vasu <user@Vasus-MacBook-AIR.local>
2018-09-05 20:29:00 +0530

    Initial commit

---
+++ src/com/sagan/javaapp/Main.java
@@ -0,0 +1,21 @@
+package com.sagan.javaapp;
+import java.util.*;
+public class Main {
+    public static void main() {
+        int i, j, n;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        s.close();
+
+        for(i=n;i>=1;i--){
+            for(j=1;j<=i;j++) {
+                if (i%2!=0) {
+                    System.out.print("0");
+                } else if (i%2==0){
+                    System.out.print("1");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/com/sagan/javaapp/main2.java
@@ -0,0 +1,7 @@
+package com.sagan.javaapp;
+public class main2 {
+	public static void main(String[] args) {
+		int x = 4 + 5 * 8 - 3 % 2;
+		System.out.println(x);
+	}
+}
