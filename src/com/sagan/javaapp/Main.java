d5433f063d3bd38f4353ce781ae5e71e6b45e643
saganx <48165569+saganx@users.noreply.github.com>
2019-03-03 14:09:03 +0530

    updated java twice

--- src/com/sagan/javaapp/Main.java
+++ src/com/sagan/javaapp/Main.java
@@ -1,12 +1,7 @@
 package com.sagan.javaapp;
 import java.util.*;
 public class Main {
-    public static void main() {
-        int i, j, n;
-        Scanner s = new Scanner(System.in);
-        n = s.nextInt();
-        s.close();
-
+    pu
         
                 }
             }
