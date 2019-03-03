7e09da3507e4702409e4f85921d5a2bc0f742ef7
saganx <48165569+saganx@users.noreply.github.com>
2019-03-03 14:06:51 +0530

    updated java again

--- src/com/sagan/javaapp/Main.java
+++ src/com/sagan/javaapp/Main.java
@@ -7,12 +7,7 @@ public class Main {
         n = s.nextInt();
         s.close();
 
-        for(i=n;i>=1;i--){
-            for(j=1;j<=i;j++) {
-                if (i%2!=0) {
-                    System.out.print("0");
-                } else if (i%2==0){
-                    System.out.print("1");
+        
                 }
             }
         }
